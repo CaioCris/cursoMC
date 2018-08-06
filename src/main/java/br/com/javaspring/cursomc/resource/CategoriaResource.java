752ef72d4cbf5cd554811f0a80a63de1b9d1b4c8
CaioCris <caiohcris@gmail.com>
2018-08-06 20:39:15 -0300

    Criação de repository(dao) e service(BO)

--- src/main/java/br/com/javaspring/cursomc/resource/CategoriaResource.java
+++ src/main/java/br/com/javaspring/cursomc/resource/CategoriaResource.java
@@ -1,30 +1,26 @@
 package br.com.javaspring.cursomc.resource;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.javaspring.cursomc.domain.Categoria;
+import br.com.javaspring.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria("Informatica");
-		Categoria cat2 = new Categoria("Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
-		
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
