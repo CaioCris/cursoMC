a87c8645bc914612f32e7a3382fb8a7213a95058
CaioCris <caiohcris@gmail.com>
2018-08-07 22:53:57 -0300

    tratando primeiros erros

---
+++ src/main/java/br/com/javaspring/cursomc/resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package br.com.javaspring.cursomc.resource.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.javaspring.cursomc.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandartError> objectNotFound(ObjectNotFoundException exception, HttpServletRequest request){
+		StandartError error = new StandartError(HttpStatus.NOT_FOUND.value(), exception.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		
+	}
+	
+}
