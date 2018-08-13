212dda42917b6b0b0e13e85cb90455ab032ecda5
CaioCris <caiohcris@gmail.com>
2018-08-13 18:05:13 -0300

    GET/PUT/DELETE em cliente

--- src/main/java/br/com/javaspring/cursomc/domain/Cliente.java
+++ src/main/java/br/com/javaspring/cursomc/domain/Cliente.java
@@ -19,34 +19,50 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import br.com.javaspring.cursomc.domain.enums.TipoCliente;
 
 @Entity
-public class Cliente implements Serializable{
+public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
-	@OneToMany(mappedBy="cliente")
+
+	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
-	
+
 	@ElementCollection
-	@CollectionTable(name="TELEFONE")
+	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-	
+
 	@JsonIgnore
-	@OneToMany(mappedBy="cliente")
+	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 		super();
 	}
+	
+	public Cliente(Integer id, String nome, String email) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+	}
 
 	public Cliente(String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipo = tipo.getCodigo();
+	}
+
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+		super();
+		this.id = id;
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
@@ -108,7 +124,7 @@ public class Cliente implements Serializable{
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
 	}
-	
+
 	public List<Pedido> getPedidos() {
 		return pedidos;
 	}
--- src/main/java/br/com/javaspring/cursomc/services/ClienteService.java
+++ src/main/java/br/com/javaspring/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package br.com.javaspring.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.javaspring.cursomc.domain.Cliente;
+import br.com.javaspring.cursomc.dto.ClienteDTO;
 import br.com.javaspring.cursomc.repositories.ClienteRepository;
+import br.com.javaspring.cursomc.services.exception.DataIntegrityException;
 import br.com.javaspring.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -21,5 +28,38 @@ public class ClienteService {
 				"Objeto não encontrado! Id: "+ id + ", Tipo: "+ Cliente.class.getName()));
 	}
 
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException error) {
+			throw new DataIntegrityException("Não é possiel excluir uma Cliente que possui Pedido");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail());
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 	
 }
