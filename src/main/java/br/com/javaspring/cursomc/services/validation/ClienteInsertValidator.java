55990f2961e4e914d00e05d4f0ef6ae269871ab6
CaioCris <caiohcris@gmail.com>
2018-08-14 16:22:33 -0300

    Validando email já cadastrado

--- src/main/java/br/com/javaspring/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/javaspring/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.javaspring.cursomc.domain.Cliente;
 import br.com.javaspring.cursomc.domain.enums.TipoCliente;
 import br.com.javaspring.cursomc.dto.ClienteNewDTO;
+import br.com.javaspring.cursomc.repositories.ClienteRepository;
 import br.com.javaspring.cursomc.resource.exception.FieldMessage;
 import br.com.javaspring.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ não é valido"));
 		}
 
+		Cliente aux =  clienteRepo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		for (FieldMessage error : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(error.getMessage()).addPropertyNode(error.getFieldName())
